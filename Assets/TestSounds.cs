ea70db7d89c915721d38d8621373f4c7926b1b3e
Samuel Hill <sjdrakeiii@gmail.com>
2019-05-14 15:21:46 -0400

    Sound Tester + Fixed Bullet Script?

---
+++ Assets/TestSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSounds : MonoBehaviour {
+
+	public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SoundManager.me.Play (clip);
+		}
+	}
+}
